4fad0ed5f341b1c0a5b48293f3513d53ad6a823c
sonal-8240 <158831506+sonal-8240@users.noreply.github.com>
2024-04-01 21:55:53 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.lang.*;
+import java.util.concurrent.atomic.*;
+import java.util.Scanner;
+public class user {
+    public static void main(String[] args) {
+        int a=78;
+        int b=90;
+        int c;
+        c=a+b;
+        System.err.println(c);
+
+
+        int d=a-b;
+        System.out.println(d);
+
+        String str="kalyani";
+        System.out.println(str);
+        for(int k=1;k<5;k++)
+{
+
+System.out.println(k);
+}
+
+
+  int i=0;
+    while(i<5){
+        System.out.println(i);
+        i++;
+    }
+
+if(a>b)
+{System.out.println("number is greater:");}
+else
+{System.out.println("number is smalll:");
+
+
+}
+
+Scanner myObj=new Scanner(System.in);
+System.out.println("Enter the user name:");
+String userName=myObj.nextLine();
+System.out.println("user name is:"+userName);
+Scanner mk= new Scanner(System.in);
+
+System.out.println("enter the first 5 no :");
+
+for( i=0;i<=5;i++){
+int roll_no=mk.nextInt();
+
+}
+
+Scanner str1=new Scanner(System.in);
+System.out.println("enter the five best friends name:");
+for(i=1;i<=5;i++)
+{
+
+    String friend_name=str1.nextLine();
+    System.out.println( " best friends name:"+friend_name);
+    
+}
+int arr[]={12,34,56,78};
+int arr2[]={1,2,3,4,5,6,3};
+int ar[]=arr.length;
+int ag[]=arr2.length;
+int l=ar+ag;
+int []p=new int [l];
+
+System.out.println("arr length is:"+arr.length);
+System.out.println("concate the two arr just try what happen in  that processs:"+arr2.concate);
+
+}}
